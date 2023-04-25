0c4da32bc2bbf8c6a87b7309f41d9f78a4f0c635
Dusan Misic <dmisic@inventage.com>
2023-04-25 11:23:18 +0200

    TDD Approach: Implement the method we just wrote a test for (happy path only for now)

--- initial/src/app/registration/registration.service.ts
+++ initial/src/app/registration/registration.service.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@angular/core';
 import { RegistrationDetails } from '../_types/registration-details.type';
+import { RouterAdapterService } from '../_services/adapters/router-adapter/router-adapter.service';
+import { UserRemoteService } from '../_services/user-remote/user-remote.service';
+import { LlamaRemoteService } from '../_services/llama-remote/llama-remote.service';
+import { appRoutesNames } from '../app.routes.names';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegistrationService {
 
-  constructor() { }
+  constructor(
+    private routerAdapterService: RouterAdapterService,
+    private userRemoteService: UserRemoteService,
+    private llamaRemoteService: LlamaRemoteService
+  ) { }
 
-  // TODO: TEST
-  registerNewUser({ user, llama }: RegistrationDetails) {
-    throw new Error('Not implemented');
+  // TODO: Handle Errors
+  async registerNewUser({ user, llama }: RegistrationDetails) {
+    const userId = await this.userRemoteService.create(user);
+
+    const llamaWithUserId = { ...llama, userId };
+
+    await this.llamaRemoteService.create(llamaWithUserId);
+
+    this.routerAdapterService.goToUrl(`/${appRoutesNames.LOGIN}`);
   }
 }
