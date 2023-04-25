2c5c29437c0ef830247a094fdab5e336767a30bd
Dusan Misic <dmisic@inventage.com>
2023-04-25 13:09:39 +0200

    TDD Approach: Implement the create method we just wrote a test for

--- initial/src/app/_services/user-remote/user-remote.service.ts
+++ initial/src/app/_services/user-remote/user-remote.service.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@angular/core';
 import { UserCredentials } from '../../_types/user-credentials.type';
+import { HttpAdapterService } from '../adapters/http-adapter/http-adapter.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserRemoteService {
 
-  constructor() { }
+  constructor(
+    private httpAdapterService: HttpAdapterService
+  ) { }
 
   // TODO: TEST
-  create(user: UserCredentials): Promise<number> {
-    throw new Error('Not implemented');
+  async create(credentials: UserCredentials): Promise<number> {
+    const tokenHolder = await this.httpAdapterService
+      .post<{ accessToken: string }>('/api/users', credentials);
+
+    return this.getUserIdFromToken(tokenHolder.accessToken);
+  }
+
+  private getUserIdFromToken(token: string): number {
+    const tokenPayload = token.split('.')[1];
+    const decodedTokenPayload = atob(tokenPayload);
+    const tokenPayloadAsObject = JSON.parse(decodedTokenPayload);
+
+    return tokenPayloadAsObject.sub;
   }
 }
